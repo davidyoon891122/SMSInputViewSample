cb84e1afc086da1a49fb4a8d958e8c17f7c3dc56
David Yoon <davidyoon@hanwha.com>
2024-05-27 15:42:59 +0900

    Initial Commit

---
+++ SMSInputSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMSInputSample
+//
+//  Created by Davidyoon on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMSInputSample/SMSInputSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMSInputSampleApp.swift
+//  SMSInputSample
+//
+//  Created by Davidyoon on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SMSInputSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
